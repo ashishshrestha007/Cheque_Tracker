f13a46e60b5d094a4d53672e2a71115d4ed0525c
krijan maharjan <krijanmaharjan31@gmail.com>
2026-02-14 19:32:05 +0545

    loginf

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def login(request):
-    
+    if request.method == "POST":
+        username = request.POST.get("staffId")
+        password = request.POST.get("password")
+        user=authenticaite(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("dashboard")
+        else:
+            messages.error(request,"Invalid username or password")
+            return redirect("login")
+
+
     return render(request,"login.html")
 
 def register(request):
+    if request.method == "POST":
+        username = request.POST.get("fullName")
+        phone=request.POST.get("phone")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirm_password=request.POST.get("confirmPassword")
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match")
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect('register')
+
+        user=User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            
+        )
+        messages.success(request, "Account created successfully")
+        return redirect('login')
     return render(request,"register.html")
 
 
 def dashboard_home(request):
+
     return render(request,"dashboard.html")
 
 def cheque_home(request):
