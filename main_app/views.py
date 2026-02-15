a31e5deeaa0af0df9444181de9a1644eebdd8fa9
krijan maharjan <krijanmaharjan31@gmail.com>
2026-02-15 10:33:04 +0545

    username setup

--- main_app/views.py
+++ main_app/views.py
@@ -16,8 +16,6 @@ def login_view(request):
         else:
             messages.error(request,"Invalid username or password")
             return redirect("login")
-
-
     return render(request,"login.html")
 
 def register(request):
